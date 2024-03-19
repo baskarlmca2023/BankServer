8285e6fba3c37405fb9cbc7c645ae9ec6bd0fe30
Baskar L <139743997+baskarlmca2023@users.noreply.github.com>
2024-03-19 23:03:02 +0530

    Update DBConfig.js

--- Config/DBConfig.js
+++ Config/DBConfig.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const DBconfig = async () => {
     try {
         // await mongoose.connect('mongodb://localhost/bank')
-        await mongoose.connect('mongodb+srv://baskar:p@$wordbaskar@cluster0.qbb81tx.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect('mongodb://127.0.0.1:27017/')
         console.log("DB connected");
     } catch (error) {
         console.log("couldn't connect DB", error);
